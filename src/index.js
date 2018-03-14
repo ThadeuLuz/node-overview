9b8dd27f729d7cf15387663a6dc5da5a4b5060c9
Thadeu Luz <thadeuluz@gmail.com>
2018-03-14 17:32:55 -0300

    Move config to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Presentation from './presentation';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import slides from './slides'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Presentation />, document.getElementById('root'));
-registerServiceWorker();
+// Import theme
+import createTheme from 'spectacle/lib/themes/default'
+import { Deck } from 'spectacle'
+
+// Require CSS
+// eslint-disable-next-line
+require('normalize.css')
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#0D6C0E', // Green
+    tertiary: '#333333' // Dark Gray
+  },
+  {
+    primary: 'Helvetica'
+    // primary: "Montserrat",
+    // secondary: "Helvetica"
+  }
+)
+
+const Presentation = () => (
+  <Deck transition={['zoom', 'slide']} transitionDuration={200} theme={theme}>
+    {slides}
+  </Deck>
+)
+
+ReactDOM.render(<Presentation />, document.getElementById('root'))
+registerServiceWorker()

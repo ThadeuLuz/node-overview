50459c695986ebdb8ec208e277f53ba5f7fc1635
Thadeu Luz <thadeuluz@gmail.com>
2018-03-16 01:23:22 -0300

    Adiciona package

---
+++ src/package.js
@@ -0,0 +1,21 @@
+const p = {
+  name: "node-overview",
+  version: "0.1.0",
+  private: true,
+  dependencies: {
+    ajv: "^6.2.1",
+    "chart.js": "^1.1.1",
+    react: "^16.2.0",
+    "react-chartjs": "^1.2.0",
+    "react-dom": "^16.2.0",
+    "spectacle-code-slide": "^0.5.2",
+    "spectacle-scripts": "2.0.0"
+  },
+  scripts: {
+    start: "react-scripts start",
+    build: "react-scripts build",
+    test: "react-scripts test --env=jsdom"
+  }
+};
+
+export default JSON.stringify(p, null, 2);
